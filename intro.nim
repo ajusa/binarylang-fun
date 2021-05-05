4986cba3433440622a4c962a5710aca9412ad2af
Arham Jain <arhamjain@gmail.com>
2021-05-05 17:40:05 -0400

    update intro to remove experimental features

--- intro.nim
+++ intro.nim
@@ -21,7 +21,7 @@ Content-Type: text/html; charset=iso-8859-1
 
 here is me"""
 nbCode:
-    echo msg
+  echo msg
 
 nbText: """
 Now there are quite a few ways to parse this. Regex is probably a pretty good cross language
@@ -41,15 +41,15 @@ Now, I can *declare* what my type looks like, and let it generate everything els
 the first line in that HTTP header, shall we?
 """
 nbCode:
-    createParser(http):
-        s: _ = "HTTP/"
-        s: version
-        s: _ = " "
-        s: code
-        s: _ = " "
-        s: msg
-        s: _ = "\n"
-    print toHTTP(msg)
+  struct(http):
+    s: _ = "HTTP/"
+    s: version
+    s: _ = " "
+    s: code
+    s: _ = " "
+    s: msg
+    s: _ = "\n"
+  print toHTTP(msg)
 
 nbText: """
 So what's going on here? First, we tell binarylang to go ahead and create a type called http, for parsing.
@@ -62,11 +62,11 @@ An underscore (_) simply signifies that we don't care enough about that value to
 Since binarylang operates on bitstreams, we turn the string into one, and then tell it to parse into an object.
 
 So, we have a functioning parser for the example header. What do we do if we wanted to generate a header though?
-    """
+  """
 
 nbCode:
-    var httpHeader = HTTP(version: "1.1", code: "200", msg: "OK")
-    echo httpHeader.fromHTTP
+  var httpHeader = HTTP(version: "1.1", code: "200", msg: "OK")
+  echo httpHeader.fromHTTP
 nbText: """Wow. It pretty much just works."""
 nbText: """
 Next are the headers. While we could proceed as we did earlier, for each of the headers, it won't quite work.
@@ -74,29 +74,29 @@ HTTP headers can be in a different order, and more importantly, they can be *any
 a sequence of headers. First, let's define a type for the header itself.
 """
 nbCode:
-    createParser(header):
-        s: name
-        s: _ = ": "
-        s: value
-        s: _ = "\n"
-    print "Server: Apache/2.2.14 (Win32)\n".toHeader
+  struct(header):
+    s: name
+    s: _ = ": "
+    s: value
+    s: _ = "\n"
+  print "Server: Apache/2.2.14 (Win32)\n".toHeader
 nbText: """
 Fantastic! We now have a way to parse a single header line. Of course, we need to handle a list of these
 somehow. Thankfully, binarylang has us covered.
 """
 
 nbCode:
-    createParser(http2):
-        s: _ = "HTTP/"
-        s: version
-        s: _ = " "
-        s: code
-        s: _ = " "
-        s: msg
-        s: _ = "\n"
-        *header: {headers}
-        s: _ = "\n"
-    print msg.toHTTP2
+  struct(http2):
+    s: _ = "HTTP/"
+    s: version
+    s: _ = " "
+    s: code
+    s: _ = " "
+    s: msg
+    s: _ = "\n"
+    *header: {headers}
+    s: _ = "\n"
+  print msg.toHTTP2
 
 nbText: """
 Hold on, what's going on here? What's up with all of the weird * and {}? Doesn't * mean a 
@@ -109,82 +109,93 @@ So, what happens is that we try to parse each header, and after parsing each one
 newline. If it is, we stop parsing headers and finish, otherwise we keep adding on to that seq. Since HTTP headers use newlines
 to delimit the different sections, this works out fine.
 
-However, working with a sequence of headers is a little strange. 
-It would be nice if it could be accessed as if it were a table in Nim,
-with a string to string mapping. Thankfully, binarylang provides `@get` and `@set`, which define procs that can
-be ran on field access or assignment. First, let's define some helper procs to convert between the two.
-    """
-nbCode:
-    proc toTable(headers: seq[Header]): auto =
-        result = newTable[string, string]()
-        for header in headers:
-            result[header.name] = header.value
-    proc toHeaders(headers: TableRef[string, string]): seq[Header] =
-        for name, value in headers:
-            result.add(Header(name: name, value: value))
-    print msg.toHTTP2.headers.toTable
-    print {"Content-Length": "10"}.newTable.toHeaders
-nbText: """
-I'm using a `TableRef` here due to the `print` macro from [treeform](https://github.com/treeform/print/blob/master/tests/test.nim#L88)
-currently being broken on normal tables.
-
-Anyway, how do we hook this up to have a nicer interface? Now, our type looks like this:
-    """
-nbCode:
-    createParser(http3):
-        s: _ = "HTTP/"
-        s: version
-        s: _ = " "
-        s: code
-        s: _ = " "
-        s: msg
-        s: _ = "\n"
-        *header {@get: _.toTable, @set: _.toHeaders}: {headers}
-        s: _ = "\n"
-    echo msg.toHTTP3.headers["Content-Length"]
-nbText: """
-That's pretty neat! All that's missing now is the actual, you know, content bit. We want to read a string
-that is as long as the content length. Here's what that looks like:
-"""
-nbCode:
-    createParser(http4):
-        s: _ = "HTTP/"
-        s: version
-        s: _ = " "
-        s: code
-        s: _ = " "
-        s: msg
-        s: _ = "\n"
-        *header {@get: _.toTable, @set: _.toHeaders}: {headers}
-        s: _ = "\n"
-        s: content(headers["Content-Length"].parseInt)
-    print msg.toHTTP4
-nbText: """We can now parse an entire HTTP response. Remember how at the start I said that we can also use
-this to serialize a full HTTP response? Let's try that.
-```nim
-    print HTTP4(version: "1.1", code: "200", msg: "OK", content: "Here I am!").fromHTTP4
-    # Error: unhandled exception: key not found: Content-Length [KeyError]
-```
-This doesn't quite work. We could provide the actual Content-Length manually, but that's
-a hassle, and somewhat error prone. Instead, we can try using `@hook` from binarylang, which
-runs code whenever a field is mutated.
-"""
-nbCode:
-    proc updateLength(headers: TableRef[string, string], length: string): auto =
-        headers["Content-Length"] = $length.len
-        return headers
-    createParser(http5):
-        s: _ = "HTTP/"
-        s: version
-        s: _ = " "
-        s: code
-        s: _ = " "
-        s: msg
-        s: _ = "\n"
-        *header {@get: _.toTable, @set: _.toHeaders}: {headers}
-        s: _ = "\n"
-        s {@hook: (headers = headers.updateLength(_))}: content(headers["Content-Length"].parseInt)
-    var response = HTTP5(version: "1.1", code: "200", msg: "OK")
-    response.content = "Here I am!"
-    echo response.fromHTTP5
+  """
+# However, working with a sequence of headers is a little strange. 
+# It would be nice if it could be accessed as if it were a table in Nim,
+# with a string to string mapping. Thankfully, binarylang provides `@get` and `@set`, which define procs that can
+# be ran on field access or assignment. First, let's define some helper procs to convert between the two.
+# nbCode:
+#   type myTable = TableRef[string, string]
+#   template tableHeaderGet(parse, parsed, output: untyped) =
+#     parse
+#     var tmp = newTable[string, string]()
+#     for header in parsed:
+#       tmp[header.name] = header.value
+#     output = tmp
+#   template tableHeaderPut(encode, encoded, output: untyped) =
+#     var tmp: seq[Header]
+#     for name, value in encoded:
+#       tmp.add(Header(name: name, value: value))
+#     output = tmp
+#     encode
+#   # proc toTable(headers: seq[Header]): auto =
+#   # proc toHeaders(headers: TableRef[string, string]): seq[Header] =
+#   #   for name, value in headers:
+#   #     result.add(Header(name: name, value: value))
+#   # print msg.toHTTP2.headers.toTable
+#   # print {"Content-Length": "10"}.newTable.toHeaders
+# nbText: """
+# I'm using a `TableRef` here due to the `print` macro from [treeform](https://github.com/treeform/print/blob/master/tests/test.nim#L88)
+# currently being broken on normal tables.
+#
+# Anyway, how do we hook this up to have a nicer interface? Now, our type looks like this:
+#   """
+# nbCode:
+#   struct(http3):
+#     s: _ = "HTTP/"
+#     s: version
+#     s: _ = " "
+#     s: code
+#     s: _ = " "
+#     s: msg
+#     s: _ = "\n"
+#     *header {tableHeader[myTable]}: {headers}
+#     # *header {@get: _.toTable, @set: _.toHeaders}: {headers}
+#     s: _ = "\n"
+#   echo msg.toHTTP3.headers["Content-Length"]
+# nbText: """
+# That's pretty neat! All that's missing now is the actual, you know, content bit. We want to read a string
+# that is as long as the content length. Here's what that looks like:
+# """
+# nbCode:
+#   struct(http4):
+#     s: _ = "HTTP/"
+#     s: version
+#     s: _ = " "
+#     s: code
+#     s: _ = " "
+#     s: msg
+#     s: _ = "\n"
+#     *header {@get: _.toTable, @set: _.toHeaders}: {headers}
+#     s: _ = "\n"
+#     s: content(headers["Content-Length"].parseInt)
+#   print msg.toHTTP4
+# nbText: """We can now parse an entire HTTP response. Remember how at the start I said that we can also use
+# this to serialize a full HTTP response? Let's try that.
+# ```nim
+#   print HTTP4(version: "1.1", code: "200", msg: "OK", content: "Here I am!").fromHTTP4
+#   # Error: unhandled exception: key not found: Content-Length [KeyError]
+# ```
+# This doesn't quite work. We could provide the actual Content-Length manually, but that's
+# a hassle, and somewhat error prone. Instead, we can try using `@hook` from binarylang, which
+# runs code whenever a field is mutated.
+# """
+# nbCode:
+#   proc updateLength(headers: TableRef[string, string], length: string): auto =
+#     headers["Content-Length"] = $length.len
+#     return headers
+#   struct(http5):
+#     s: _ = "HTTP/"
+#     s: version
+#     s: _ = " "
+#     s: code
+#     s: _ = " "
+#     s: msg
+#     s: _ = "\n"
+#     *header {@get: _.toTable, @set: _.toHeaders}: {headers}
+#     s: _ = "\n"
+#     s {@hook: (headers = headers.updateLength(_))}: content(headers["Content-Length"].parseInt)
+#   var response = HTTP5(version: "1.1", code: "200", msg: "OK")
+#   response.content = "Here I am!"
+#   echo response.fromHTTP5
 nbSave
--- logs.nim
+++ logs.nim
@@ -19,36 +19,36 @@ between strings, therefore it is pretty straightforward to parse it with
 binarylang.
 """
 nbCode:
-    struct(record):
-        s: ip
-        s: _ = " - - "
-        s: serverTime
-        s: _ = """ "GET /?time="""
-        s: clientTime
-        s: _ = "&random="
-        s: random
-        s: _ = "&webrtc="
-        s: webrtc
-        s: _ = "&websocket="
-        s: websocket
-        s: _ = """ HTTP/1.1" 200 """ 
-        s: _
-        s: _ = """ "https://588.lan.wtf/start/" """"
-        s: useragent
-        s: _ = "\"\n"
-    print log.toRecord
-    echo Record(
-        ip: "localhost", serverTime: "500", clientTime: "600", random: "23",
-        webrtc: "true", websocket: "true", useragent: "Chrome"
-    ).fromRecord
+  struct(record):
+    s: ip
+    s: _ = " - - "
+    s: serverTime
+    s: _ = """ "GET /?time="""
+    s: clientTime
+    s: _ = "&random="
+    s: random
+    s: _ = "&webrtc="
+    s: webrtc
+    s: _ = "&websocket="
+    s: websocket
+    s: _ = """ HTTP/1.1" 200 """ 
+    s: _
+    s: _ = """ "https://588.lan.wtf/start/" """"
+    s: useragent
+    s: _ = "\"\n"
+  print log.toRecord
+  echo Record(
+    ip: "localhost", serverTime: "500", clientTime: "600", random: "23",
+    webrtc: "true", websocket: "true", useragent: "Chrome"
+  ).fromRecord
 nbText: """
 And there you have it. We can not only parse logs, but now we can also generate
 them if we wanted to. Lastly, we'll parse the entire thing into a sequence using binarylang.
 """
 nbCode:
-    struct(logs):
-        *record: records{s.atEnd}
-    print log.toLogs.records[0..2]
+  struct(logs):
+    *record: records{s.atEnd}
+  print log.toLogs.records[0..2]
 nbText: """
 We tell binarylang to keep parsing each log line until we hit the end
 of the file (that is what the curly braces after the field indicate.)
